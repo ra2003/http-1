f0956051451bcf118e03aed4a32a1aff232c2ab0
Embedthis Software <dev@embedthis.com>
2013-04-26 08:53:07 -0700

    Fix mvc-simple route

--- src/route.c
+++ src/route.c
@@ -2399,8 +2399,6 @@ PUBLIC void httpAddRouteSet(HttpRoute *parent, cchar *set)
     } else if (scaselessmatch(set, "mvc-simple")) {
         httpAddHomeRoute(parent);
         httpAddStaticRoute(parent);
-        httpDefineRoute(parent, "default", NULL, "^/{controller}(~/{action}~)", "${controller}-${action}", 
-            "${controller}.c");
 
     } else if (scaselessmatch(set, "mvc")) {
         httpAddHomeRoute(parent);

b58b0a28566cf49a6b1f9d357d56a17aa17708c1
Embedthis Software <dev@embedthis.com>
2013-11-12 19:19:48 -0800

    Dont log routes with name of 'unused'

--- src/host.c
+++ src/host.c
@@ -137,7 +137,7 @@ static void printRoute(HttpRoute *route, int next, bool full)
     cchar       *methods, *pattern, *target, *index;
     int         nextIndex;
 
-    if (smatch(route->name, "unused")) {
+    if (smatch(route->name, "unused") || (route->flags & HTTP_ROUTE_DISABLED)) {
         return;
     }
     methods = httpGetRouteMethods(route);

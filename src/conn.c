0b6d476f2e475da09d946f3bec5bf38749bd06d8
Embedthis Software <dev@embedthis.com>
2013-11-18 14:34:17 -0800

    Harden mprStealConn

--- src/conn.c
+++ src/conn.c
@@ -506,8 +506,9 @@ PUBLIC MprSocket *httpStealConn(HttpConn *conn)
 {
     MprSocket   *sock;
 
-    sock = conn->sock;
-    mprRemoveSocketHandler(sock);
+    if ((sock = conn->sock) != 0) {
+        mprRemoveSocketHandler(sock);
+    }
     conn->sock = 0;
 
     if (conn->http) {

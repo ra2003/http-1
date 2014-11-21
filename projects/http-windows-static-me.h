1afae700e32ab9f4655d0fa0155733e2a3431155
Embedthis Software <dev@embedthis.com>
2014-11-21 14:02:13 -0800

    REGEN: windows projects to use est

--- projects/http-windows-static-me.h
+++ projects/http-windows-static-me.h
@@ -184,7 +184,7 @@
 
 /* Profile */
 #ifndef ME_CONFIG_CMD
-    #define ME_CONFIG_CMD "me -d -q -platform windows-x86-static -static -configure . -with openssl -gen vs"
+    #define ME_CONFIG_CMD "me -d -q -platform windows-x86-static -static -configure . -with est -gen vs"
 #endif
 #ifndef ME_HTTP_PRODUCT
     #define ME_HTTP_PRODUCT 1
@@ -215,7 +215,7 @@
     #define ME_COM_CC 1
 #endif
 #ifndef ME_COM_EST
-    #define ME_COM_EST 0
+    #define ME_COM_EST 1
 #endif
 #ifndef ME_COM_LIB
     #define ME_COM_LIB 1
@@ -227,7 +227,7 @@
     #define ME_COM_MPR 1
 #endif
 #ifndef ME_COM_OPENSSL
-    #define ME_COM_OPENSSL 1
+    #define ME_COM_OPENSSL 0
 #endif
 #ifndef ME_COM_OSDEP
     #define ME_COM_OSDEP 1

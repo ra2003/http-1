ae2bad449c81031baa4e0bdac00fda01053e26e5
Embedthis Software <dev@embedthis.com>
2015-01-16 08:05:13 -0800

    DEV: add include config file support.

--- src/config.c
+++ src/config.c
@@ -52,6 +52,18 @@ PUBLIC void httpParseError(HttpRoute *route, cchar *fmt, ...)
 }
 
 
+PUBLIC void httpParseWarn(HttpRoute *route, cchar *fmt, ...)
+{
+    va_list     args;
+    char        *msg;
+
+    va_start(args, fmt);
+    msg = sfmtv(fmt, args);
+    mprLog("warn http config", 1, "%s", msg);
+    va_end(args);
+}
+
+
 /*
     Convert a JSON string to a space-separated C string
  */
@@ -115,9 +127,34 @@ static void blendMode(HttpRoute *route, MprJson *config)
 }
 
 
+PUBLIC int parseInclude(HttpRoute *route, MprJson *config, MprJson *inc)
+{
+    MprJson     *child, *obj;
+    MprList     *files;
+    cchar       *data, *errorMsg, *path;
+    int         ji, next;
+
+    for (ITERATE_CONFIG(route, inc, child, ji)) {
+        files = mprGlobPathFiles(".", child->value, MPR_PATH_NO_DIRS | MPR_PATH_RELATIVE);
+        for (ITERATE_ITEMS(files, path, next)) {
+            if ((data = mprReadPathContents(path, NULL)) == 0) {
+                httpParseError(route, "Cannot read configuration from \"%s\"", path);
+                return MPR_ERR_CANT_READ;
+            }
+            if ((obj = mprParseJsonEx(data, 0, 0, 0, &errorMsg)) == 0) {
+                httpParseError(route, "Cannot parse %s: error %s", path, errorMsg);
+                return MPR_ERR_CANT_READ;
+            }
+            mprBlendJson(config, obj, MPR_JSON_COMBINE);
+        }
+    }
+    return 0;
+}
+
+
 PUBLIC int parseFile(HttpRoute *route, cchar *path)
 {
-    MprJson     *config;
+    MprJson     *config, *obj;
     cchar       *data, *errorMsg;
 
     if ((data = mprReadPathContents(path, NULL)) == 0) {
@@ -127,6 +164,9 @@ PUBLIC int parseFile(HttpRoute *route, cchar *path)
     if ((config = mprParseJsonEx(data, 0, 0, 0, &errorMsg)) == 0) {
         mprLog("error http config", 0, "Cannot parse %s: error %s", path, errorMsg);
         return MPR_ERR_CANT_READ;
+    }
+    if ((obj = mprGetJsonObj(config, "include")) != 0) {
+        parseInclude(route, config, obj);
     }
 #if DEPRECATE || 1
 {
@@ -177,7 +217,8 @@ PUBLIC int httpFinalizeConfig(HttpRoute *route)
 {
     HttpHost    *host;
     HttpRoute   *rp;
-    MprJson     *obj, *mappings, *routes;
+    MprJson     *apps, *obj, *mappings, *routes;
+    cchar       *home;
     int         nextHost, nextRoute;
 
     if (route->error) {
@@ -190,13 +231,17 @@ PUBLIC int httpFinalizeConfig(HttpRoute *route)
         parseRoutes(route, "http.routes", routes);
     }
 
+    if ((apps = mprGetJsonObj(route->config, "http.apps")) != 0) {
+        parseRoutes(route, "http.apps", apps);
+    }
+
     /*
         Create a subset, optimized configuration to send to the client
      */
     if ((obj = mprGetJsonObj(route->config, "http.client.mappings")) == 0) {
 #if DEPRECATED || 1
         if ((obj = mprGetJsonObj(route->config, "http.mappings")) != 0) {
-            mprLog("warn http", 0, "Using deprecated http.mappings. use http.client.mappings instead");
+            mprLog("warn http", 0, "Using deprecated http.mappings. use http.client.mappings instead.");
         }
 #endif
     }
@@ -225,6 +270,29 @@ PUBLIC int httpFinalizeConfig(HttpRoute *route)
             }
         }
     }
+#if ME_UNIX_LIKE
+    if ((home = mprGetJson(route->config, "http.server.chroot")) != 0) {
+        home = httpMakePath(route, 0, home);
+        if (chdir(home) < 0) {
+            httpParseError(route, "Cannot change working directory to %s", home);
+            return MPR_ERR_BAD_STATE;
+        }
+        if (route->flags & HTTP_ROUTE_UTILITY) {
+            /* Not running a web server but rather a utility like the "esp" generator program */
+            mprLog("info http config", 2, "Change directory to: \"%s\"", home);
+        } else {
+            if (chroot(home) < 0) {
+                if (errno == EPERM) {
+                    httpParseError(route, "Must be super user to use chroot\n");
+                } else {
+                    httpParseError(route, "Cannot change change root directory to %s, errno %d\n", home, errno);
+                }
+            } else {
+                mprLog("info http config", 2, "Chroot to: \"%s\"", home);
+            }
+        }
+    }
+#endif
     if (route->error) {
         route->config = 0;
         return MPR_ERR_BAD_STATE;
@@ -285,10 +353,12 @@ PUBLIC void httpParseAll(HttpRoute *route, cchar *key, MprJson *prop)
 }
 
 
+#if DEPRECATED || 1
 static void parseApp(HttpRoute *route, cchar *key, MprJson *prop)
 {
     httpParseAll(route, 0, prop);
 }
+#endif
 
 
 static void parseDirectories(HttpRoute *route, cchar *key, MprJson *prop)
@@ -303,6 +373,40 @@ static void parseDirectories(HttpRoute *route, cchar *key, MprJson *prop)
             httpSetRouteHome(route, child->value);
         }
         httpSetDir(route, child->name, child->value);
+    }
+}
+
+
+static void parseAliases(HttpRoute *route, cchar *key, MprJson *prop)
+{
+    MprJson     *child;
+    HttpRoute   *alias;
+    MprPath     info;
+    cchar       *path, *prefix;
+    int         ji;
+
+    for (ITERATE_CONFIG(route, prop, child, ji)) {
+        prefix = child->name;
+        path = child->value;
+        if (!path || !prefix) {
+            httpParseError(route, "Alias is missing path or prefix properties");
+            break;
+        }
+        mprGetPathInfo(path, &info);
+        if (info.isDir) {
+            alias = httpCreateAliasRoute(route, prefix, path, 0);
+            if (sends(prefix, "/")) {
+                httpSetRoutePattern(alias, sfmt("^%s(.*)$", prefix), 0);
+            } else {
+                /* Add a non-capturing optional trailing "/" */
+                httpSetRoutePattern(alias, sfmt("^%s(?:/)*(.*)$", prefix), 0);
+            }
+            httpSetRouteTarget(alias, "run", "$1");
+        } else {
+            alias = httpCreateAliasRoute(route, sjoin("^", prefix, NULL), 0, 0);
+            httpSetRouteTarget(alias, "run", path);
+        }
+        httpFinalizeRoute(alias);
     }
 }
 
@@ -515,6 +619,18 @@ static void parseCache(HttpRoute *route, cchar *key, MprJson *prop)
 }
 
 
+static void parseCgiEscape(HttpRoute *route, cchar *key, MprJson *prop)
+{
+    httpSetRouteEnvEscape(route, prop->type & MPR_JSON_TRUE);
+}
+
+
+static void parseCgiPrefix(HttpRoute *route, cchar *key, MprJson *prop)
+{
+    httpSetRouteEnvPrefix(route, prop->value);
+}
+
+
 static void parseCompress(HttpRoute *route, cchar *key, MprJson *prop)
 {
     if (smatch(prop->value, "true")) {
@@ -573,7 +689,7 @@ static void parseFormatsResponse(HttpRoute *route, cchar *key, MprJson *prop)
 static void parseHandler(HttpRoute *route, cchar *key, MprJson *prop)
 {
     if (httpSetRouteHandler(route, prop->value) < 0) {
-        httpParseError(route, "Cannot add handler %s", prop->value);
+        httpParseError(route, "Cannot add handler \"%s\"", prop->value);
     }
 }
 
@@ -719,6 +835,30 @@ static void parseLimitsFiles(HttpRoute *route, cchar *key, MprJson *prop)
     mprSetFilesLimit(getint(prop->value));
 }
 
+
+static void parseLimitsDepletion(HttpRoute *route, cchar *key, MprJson *prop)
+{
+    cchar   *policy;
+    int     flags;
+
+    flags = MPR_ALLOC_POLICY_EXIT;
+    policy = prop->value;
+
+    if (scmp(policy, "restart") == 0) {
+#if VXWORKS
+        flags = MPR_ALLOC_POLICY_RESTART;
+#else
+        /* Appman will restart */
+        flags = MPR_ALLOC_POLICY_EXIT;
+#endif
+    } else if (scmp(policy, "continue") == 0) {
+        flags = MPR_ALLOC_POLICY_PRUNE;
+    } else {
+        httpParseError(route, "Unknown limit depletion policy '%s'", policy);
+        return;
+    }
+    mprSetMemPolicy(flags);
+}
 
 static void parseLimitsKeepAlive(HttpRoute *route, cchar *key, MprJson *prop)
 {
@@ -885,8 +1025,7 @@ static void parsePipelineFilters(HttpRoute *route, cchar *key, MprJson *prop)
 #endif
         }
         if (httpAddRouteFilter(route, name, extensions, flags) < 0) {
-            httpParseError(route, "Cannot add filter %s", name);
-            break;
+            httpParseWarn(route, "Cannot add filter %s", name);
         }
     }
 }
@@ -907,14 +1046,13 @@ static void parsePipelineHandlers(HttpRoute *route, cchar *key, MprJson *prop)
 
     if (prop->type & MPR_JSON_STRING) {
         if (httpAddRouteHandler(route, prop->value, "") < 0) {
-            httpParseError(route, "Cannot add handler %s", prop->value);
+            httpParseWarn(route, "Handler \"%s\" is not available", prop->name);
         }
 
     } else {
         for (ITERATE_CONFIG(route, prop, child, ji)) {
             if (httpAddRouteHandler(route, child->name, getList(child)) < 0) {
-                httpParseError(route, "Cannot add handler %s", child->name);
-                break;
+                httpParseWarn(route, "Handler \"%s\" is not available", child->name);
             }
         }
     }
@@ -1098,21 +1236,13 @@ static void parseScheme(HttpRoute *route, cchar *key, MprJson *prop)
 }
 
 
-/*
-    The server collection is only parsed for utilities and not if hosted
- */
-static void parseServer(HttpRoute *route, cchar *key, MprJson *prop)
-{
-    if (route->http->flags & HTTP_UTILITY) {
-        httpParseAll(route, key, prop);
-    }
-}
-
-
 static void parseServerAccount(HttpRoute *route, cchar *key, MprJson *prop)
 {
     cchar       *value;
 
+    if (route->flags & HTTP_ROUTE_HOSTED) {
+        return;
+    }
     if ((value = mprReadJson(prop, "user")) != 0) {
         if (!smatch(value, "_unchanged_") && !mprGetDebugMode()) {
             httpSetGroupAccount(value);
@@ -1123,39 +1253,6 @@ static void parseServerAccount(HttpRoute *route, cchar *key, MprJson *prop)
             httpSetUserAccount(value);
         }
     }
-}
-
-
-static void parseServerChroot(HttpRoute *route, cchar *key, MprJson *prop)
-{
-#if ME_UNIX_LIKE
-    char        *home;
-
-    if (!prop->value && !*prop->value) {
-        return;
-    }
-    home = httpMakePath(route, 0, prop->value);
-    if (chdir(home) < 0) {
-        httpParseError(route, "Cannot change working directory to %s", home);
-        return;
-    }
-    if (route->http->flags & HTTP_UTILITY) {
-        /* Not running a web server but rather a utility like the "esp" generator program */
-        mprLog("info http config", 2, "Change directory to: \"%s\"", home);
-    } else {
-        if (chroot(home) < 0) {
-            if (errno == EPERM) {
-                httpParseError(route, "Must be super user to use chroot\n");
-            } else {
-                httpParseError(route, "Cannot change change root directory to %s, errno %d\n", home, errno);
-            }
-            return;
-        }
-        mprLog("info http config", 2, "Chroot to: \"%s\"", home);
-    }
-#else
-    mprLog("info http config", 2, "Chroot directive not supported on this operating system\n");
-#endif
 }
 
 
@@ -1178,6 +1275,9 @@ static void parseServerListen(HttpRoute *route, cchar *key, MprJson *prop)
     char            *ip;
     int             ji, port, secure;
 
+    if (route->flags & HTTP_ROUTE_HOSTED) {
+        return;
+    }
     host = route->host;
     for (ITERATE_CONFIG(route, prop, child, ji)) {
         mprParseSocketAddress(child->value, &ip, &port, &secure, 80);
@@ -1231,6 +1331,9 @@ static void parseServerLog(HttpRoute *route, cchar *key, MprJson *prop)
     ssize       size;
     int         level, anew, backup;
 
+    if (route->flags & HTTP_ROUTE_HOSTED) {
+        return;
+    }
     if (mprGetCmdlineLogging()) {
         mprLog("warn http config", 4, "Already logging. Ignoring log configuration");
         return;
@@ -1265,6 +1368,43 @@ static void parseServerLog(HttpRoute *route, cchar *key, MprJson *prop)
     }
 }
 
+
+static void parseServerModules(HttpRoute *route, cchar *key, MprJson *prop)
+{
+    MprModule   *module;
+    MprJson     *child;
+    cchar       *entry, *name, *path;
+    int         ji;
+
+    for (ITERATE_CONFIG(route, prop, child, ji)) {
+        name = mprGetJson(child, "name");
+        path = mprGetJson(child, "path");
+        if (!name) {
+            name = path;
+        }
+        if (!path) {
+            path = sjoin("libmod_", name, ME_SHOBJ, NULL);
+        }
+        if ((module = mprLookupModule(name)) != 0) {
+#if ME_STATIC
+            mprLog("info http config", 2, "Activating module (Builtin) %s", name);
+#endif
+            continue;
+        }
+        entry = sfmt("http%sInit", stitle(name));
+        module = mprCreateModule(name, path, entry, HTTP);
+
+        if (mprLoadModule(module) < 0) {
+#if DEPRECATED || 1
+            module->entry = sfmt("ma%sInit", stitle(name));
+            if (mprLoadModule(module) < 0) {
+                httpParseError(route, "Cannot load module: %s", path);
+            }
+#endif
+            break;
+        }
+    }
+}
 
 static void parseServerMonitors(HttpRoute *route, cchar *key, MprJson *prop)
 {
@@ -1314,6 +1454,9 @@ static void parseSsl(HttpRoute *route, cchar *key, MprJson *prop)
 {
     HttpRoute   *parent;
 
+    if (route->flags & HTTP_ROUTE_HOSTED) {
+        return;
+    }
     parent = route->parent;
     if (route->ssl == 0) {
         if (parent && parent->ssl) {
@@ -1576,17 +1719,6 @@ static void parseXsrf(HttpRoute *route, cchar *key, MprJson *prop)
 }
 
 
-static void parseInclude(HttpRoute *route, cchar *key, MprJson *prop)
-{
-    MprJson     *child;
-    int         ji;
-
-    for (ITERATE_CONFIG(route, prop, child, ji)) {
-        parseFile(route, child->value);
-    }
-}
-
-
 PUBLIC int httpInitParser()
 {
     HTTP->parsers = mprCreateHash(0, MPR_HASH_STATIC_VALUES);
@@ -1594,8 +1726,11 @@ PUBLIC int httpInitParser()
     /*
         Parse callbacks keys are specified as they are defined in the json files
      */
+#if DEPRECATED || 1
     httpAddConfig("app", parseApp);
+#endif
     httpAddConfig("http", parseHttp);
+    httpAddConfig("http.aliases", parseAliases);
     httpAddConfig("http.auth", parseAuth);
     httpAddConfig("http.auth.auto", httpParseAll);
     httpAddConfig("http.auth.auto.name", parseAuthAutoName);
@@ -1612,6 +1747,9 @@ PUBLIC int httpInitParser()
     httpAddConfig("http.auth.type", parseAuthType);
     httpAddConfig("http.auth.users", parseAuthUsers);
     httpAddConfig("http.cache", parseCache);
+    httpAddConfig("http.cgi", httpParseAll);
+    httpAddConfig("http.cgi.escape", parseCgiEscape);
+    httpAddConfig("http.cgi.prefix", parseCgiPrefix);
 #if KEEP
     httpAddConfig("http.content", httpParseAll);
     httpAddConfig("http.content.combine", parseContentCombine);
@@ -1646,6 +1784,7 @@ PUBLIC int httpInitParser()
     httpAddConfig("http.limits.chunk", parseLimitsChunk);
     httpAddConfig("http.limits.clients", parseLimitsClients);
     httpAddConfig("http.limits.connections", parseLimitsConnections);
+    httpAddConfig("http.limits.depletion", parseLimitsDepletion);
     httpAddConfig("http.limits.keepAlive", parseLimitsKeepAlive);
     httpAddConfig("http.limits.files", parseLimitsFiles);
     httpAddConfig("http.limits.memory", parseLimitsMemory);
@@ -1675,12 +1814,12 @@ PUBLIC int httpInitParser()
     httpAddConfig("http.resources", parseResources);
     httpAddConfig("http.scheme", parseScheme);
 
-    httpAddConfig("http.server", parseServer);
+    httpAddConfig("http.server", httpParseAll);
     httpAddConfig("http.server.account", parseServerAccount);
-    httpAddConfig("http.server.chroot", parseServerChroot);
     httpAddConfig("http.server.defenses", parseServerDefenses);
     httpAddConfig("http.server.listen", parseServerListen);
     httpAddConfig("http.server.log", parseServerLog);
+    httpAddConfig("http.server.modules", parseServerModules);
     httpAddConfig("http.server.monitors", parseServerMonitors);
     httpAddConfig("http.server.ssl", parseSsl);
     httpAddConfig("http.server.ssl.authority", httpParseAll);
@@ -1712,7 +1851,6 @@ PUBLIC int httpInitParser()
     httpAddConfig("http.update", parseUpdate);
     httpAddConfig("http.xsrf", parseXsrf);
     httpAddConfig("directories", parseDirectories);
-    httpAddConfig("include", parseInclude);
 
     return 0;
 }

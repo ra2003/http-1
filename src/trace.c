dbe82fc605b25d8bc542421e1d27fae76b0a0932
Embedthis Software <dev@embedthis.com>
2015-04-04 14:06:21 -0700

    FIX: assert

--- src/trace.c
+++ src/trace.c
@@ -552,7 +552,9 @@ PUBLIC void httpWriteTraceLogFile(HttpTrace *trace, cchar *buf, ssize len)
         unlock(trace);
         return;
     }
-    mprWriteFile(trace->file, buf, len);
+	if (trace->file) {
+		mprWriteFile(trace->file, buf, len);
+	}
     unlock(trace);
 }
 

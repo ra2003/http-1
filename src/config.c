b703c0b41874f30e3d5a66e7c6bd56ff3e0dd4f8
Embedthis Software <dev@embedthis.com>
2015-06-03 10:52:17 -0700

    DEV: Remove configure DH params.

--- src/config.c
+++ src/config.c
@@ -1587,21 +1587,6 @@ static void parseSslCiphers(HttpRoute *route, cchar *key, MprJson *prop)
 }
 
 
-static void parseSslDh(HttpRoute *route, cchar *key, MprJson *prop)
-{
-    cchar   *path;
-
-    path = httpExpandRouteVars(route, prop->value);
-    if (path && *path) {
-        if (!mprPathExists(path, R_OK)) {
-            httpParseError(route, "Cannot find file %s", path);
-        } else {
-            mprSetSslDhFile(route->ssl, path);
-        }
-    }
-}
-
-
 static void parseSslKey(HttpRoute *route, cchar *key, MprJson *prop)
 {
     cchar   *path;
@@ -2017,7 +2002,6 @@ PUBLIC int httpInitParser()
     httpAddConfig("http.server.ssl.authority.directory", parseSslAuthorityDirectory);
     httpAddConfig("http.server.ssl.certificate", parseSslCertificate);
     httpAddConfig("http.server.ssl.ciphers", parseSslCiphers);
-    httpAddConfig("http.server.ssl.dh", parseSslDh);
     httpAddConfig("http.server.ssl.key", parseSslKey);
     httpAddConfig("http.server.ssl.provider", parseSslProvider);
     httpAddConfig("http.server.ssl.protocols", parseSslProtocols);

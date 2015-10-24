91d8f38e7378f553fc756a7c09233a4d3266f407
Embedthis Software <dev@embedthis.com>
2015-10-23 17:46:13 -0700

    FIX: ShowErrors being set to true regardless

--- src/config.c
+++ src/config.c
@@ -117,25 +117,24 @@ static void blendMode(HttpRoute *route, MprJson *config)
     mode = mprGetJson(route->config, "pak.mode");
     if (!mode) {
         mode = mprGetJson(config, "pak.mode");
-        if (!mode) {
-            mode = sclone("debug");
+    }
+    if (mode) {
+        if ((route->debug = smatch(mode, "debug")) != 0) {
+            httpSetRouteShowErrors(route, 1);
+            route->keepSource = 1;
         }
-    }
-    route->mode = mode;
-    if ((route->debug = smatch(mode, "debug")) != 0) {
-        httpSetRouteShowErrors(route, 1);
-        route->keepSource = 1;
-    }
-    /*
-        Http uses top level modes
-        Pak uses top level pak.modes
-     */
-    if ((modeObj = mprGetJsonObj(config, sfmt("modes.%s", mode))) == 0) {
-        modeObj = mprGetJsonObj(config, sfmt("pak.modes.%s", mode));
-    }
-    if (modeObj) {
-        mprBlendJson(route->config, modeObj, MPR_JSON_OVERWRITE);
-        httpParseAll(route, 0, modeObj);
+        /*
+            Http uses top level modes
+            Pak uses top level pak.modes
+         */
+        if ((modeObj = mprGetJsonObj(config, sfmt("modes.%s", mode))) == 0) {
+            modeObj = mprGetJsonObj(config, sfmt("pak.modes.%s", mode));
+        }
+        if (modeObj) {
+            mprBlendJson(route->config, modeObj, MPR_JSON_OVERWRITE);
+            httpParseAll(route, 0, modeObj);
+        }
+        route->mode = mode;
     }
 }
 
